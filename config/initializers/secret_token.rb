232c227c51d896beebfe4bad2d6846e5dfc81cdb
dreamingfurther <dreamingfurther@gmail.com>
2013-12-11 21:46:17 -0500

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TddFeature::Application.config.secret_key_base = '7c14ef7df67e19194ef303407125c7ecdc23ec737a8a0d151740dfc4b1fb415e426ce0a789a9f6ea72e3ceac7379917adc4180025c9080f0954d3de1422ba6f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TddFeature::Application.config.session_store :cookie_store, key: '_tdd_feature_session'
